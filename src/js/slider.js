8a50d0036717026e4e6a230eab7528f6b0bb4c37
legioner7 <makar.anton.work@gmail.com>
2023-11-08 19:21:38 +0100

    slider

---
+++ src/js/slider.js
@@ -0,0 +1,14 @@
+const slider = new Swiper('.slider-container', {
+    direction: 'horizontal',
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
