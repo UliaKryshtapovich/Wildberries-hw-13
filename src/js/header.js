27aaabf661e0a4b2ba6d6a442511c855f924fd58
Julia <uliakrishtapovich@gmail.com>
2023-11-09 12:51:40 +0100

    added-modal_render_img

--- src/js/header.js
+++ src/js/header.js
@@ -6,6 +6,7 @@
 const headerCartBascket = document.querySelector(".header-cart");
 const headerModal = document.getElementById("header-modal");
 const headerModalCard = document.querySelector(".header-modal_card");
+const headerModalRender = document.getElementById("header-modal_render");
 
 // слушатели событий
 headerCartBascket.addEventListener("click", openCart);
@@ -13,17 +14,17 @@ document.addEventListener("click", closeCartOutside);
 
 // oткрываем модальное окно при нажатии на .header-cart
 function openCart(event) {
-    headerModal.style.display = "block";
-    event.stopPropagation(); // Эта строка предотвращает всплытие события, чтобы не вызывать `closeCartOutside` сразу же после открытия
-  }
-  
+  headerModal.style.display = "block";
+  event.stopPropagation();
+}
 
 // закрываем модальное окно, если пользователь кликает за пределами окна
 function closeCartOutside(event) {
-    if (headerModal.style.display === "block" && event.target !== headerModal && !headerModal.contains(event.target)) {
-      headerModal.style.display = "none";
-    }
-    
+  if (
+    headerModal.style.display === "block" &&
+    event.target !== headerModal &&
+    !headerModal.contains(event.target)
+  ) {
+    headerModal.style.display = "none";
   }
-  
-  
+}
