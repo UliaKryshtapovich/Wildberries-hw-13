59b6db20ef48a7b35966a396f67e9d134c013e3c
Julia <uliakrishtapovich@gmail.com>
2023-11-11 16:13:13 +0100

    fixed_slider

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,12 +1,13 @@
 const swiper = new Swiper('.swiper', {
   direction: 'horizontal',
   loop: true,
- 
+  autoplay: {
+    delay: 4000, 
+  },
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
